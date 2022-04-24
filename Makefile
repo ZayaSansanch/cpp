6005b64bc47344971dfe0248fdff38760830e0c1
Lev111111 <98773015+ZayaSansanch@users.noreply.github.com>
2022-04-24 13:09:22 +0700

    add tests files

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
-build:
+.PHONY: test var
+
+test:
 	g++ test.cpp -o test && ./test
-	 
+
+var:
+	g++ var.cpp -o var && ./var
---
+++ var.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1, num3;
+    int num2; 
+
+    // num1 = 1;
+    num1 = 1.3;
+    num2 = 2;
+
+    num3 = float(num1) / float(num2);
+
+    cout << num3 << "\n";
+}

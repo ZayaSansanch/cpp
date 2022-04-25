ccc200339934c5cfa12ebf91dc44f2a2ca89f997
Lev111111 <98773015+ZayaSansanch@users.noreply.github.com>
2022-04-25 18:33:44 +0700

    add

--- testirtwo.cpp
+++ testirtwo.cpp
@@ -3,22 +3,36 @@ using namespace std;
 
 void testone() {
     int testonec = 0;
-    string otv, otvtwo;
-    cout << "Заися ..." << "\n";
+    string otv, otvtwo, otvtree, otvfour;
+    cout << "\n" << "Заися ..." << "\n";
     cout << "A: Сусаныч         B: Сасаныч          C: Сунсаныч         D: Сансаныч" << "\n";
     cin >> otv;
     if (otv == "d" || otv == "D") {
         testonec += 1;
     }
 
-    cout << "Какая игрушка есть у заи?" << "\n";
+    cout << "\n" << "Какая игрушка есть у заи?" << "\n";
     cout << "A: Пантера         B: Панда        C: Коала        D: Геккончик" << "\n";
     cin >> otvtwo;
     if (otvtwo == "c" || otvtwo == "C") {
         testonec += 1;
     }
 
-    cout << "Вы набрали " << testonec << " очков!" << "\n";
+    cout << "\n" << "Придагается ..." << "\n";
+    cout << "A: Петух         B: Панда       C: Дедушка        D: Зая" << "\n";
+    cin >> otvtree;
+    if (otvtree == "a" || otvtree == "A") {
+        testonec += 1;
+    }
+
+    cout << "\n" << "Зая ... когда я говорю с заей по видео-связи" << "\n";
+    cout << "A: Лает         B: Крутит головой          C: Пикает        D: Вырывается из рук" << "\n";
+    cin >> otvfour;
+    if (otvfour == "b" || otvfour == "B") {
+        testonec += 1;
+    }
+
+    cout << "Вы набрали " << testonec << "/4 " << "очков!" << "\n";
 }
 
 void vari() {
@@ -28,7 +42,7 @@ void vari() {
     cin >> var;
 
     if (var == "a" or var == "A") {
-        cout << "Заися!" << "\n" << "\n";
+        cout << "Заися!" << "\n";
         testone();
     } else {
         cout << "Wat you type?" << "\n" << "\n";
